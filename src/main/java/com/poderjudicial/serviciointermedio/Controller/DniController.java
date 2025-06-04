8893d426cf8b011dda2681e141fe4133933f9f22
lmariano22 <LMARIANO@PJ.GOB.PE>
2025-06-04 08:25:23 -0500

    Update DniController.java

--- src/main/java/com/poderjudicial/serviciointermedio/Controller/DniController.java
+++ src/main/java/com/poderjudicial/serviciointermedio/Controller/DniController.java
@@ -13,7 +13,7 @@ public class DniController {
 	
 	@GetMapping("/consulta-dni/{dni}")
     public ResponseEntity<Map<String, Object>> consultarDni(@PathVariable String dni) {
-        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imx1aXNhbmdlbG1hcmlhbm9qYXZpZXJAZ21haWwuY29tIn0.FJ_BsNvRo0prJdu_SPncFZUamn539ciFIc6dOyoAVCM";
+        String token = "https://dniruc.apisperu.com/api/v1/dni/12345678?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImluZm9ybWF0aWNhX3ZlbnRhbmlsbGFAcGouZ29iLnBlIn0.TcXifDWUiXG5WvWgHtgLZm7dB4ACMBxvkyOGx2TjY9w";
         String url = "https://dniruc.apisperu.com/api/v1/dni/" + dni + "?token=" + token;
 
         Map<String, Object> response = new HashMap<>();
